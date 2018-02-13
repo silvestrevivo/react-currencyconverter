97a51a391862ac1caf689a69a55273848ca63717
silvestrevivo <silvestrevivomillan@gmail.com>
2018-02-13 08:20:39 +0100

    layout dynamic

--- src/js/App.js
+++ src/js/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import image from '../images/cash-calculator.svg'
+import image from '../images/cash-calculator.svg';
+import data from './data/Data';
+
 
 class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-
+    this.state = {
+      data: data.currencies,
+      name: 'United States',
+      rate: 0.7041,
+      code: 'USD',
+      sign: '$'
+    }
   }
-  render(){
+
+  handleChange(event) {
+    let target = event.target;
+    this.setState({
+      name: target.options[target.selectedIndex].value,
+      code: target.options[target.selectedIndex].dataset.code,
+      rate: target.options[target.selectedIndex].dataset.sell,
+      sign: target.options[target.selectedIndex].dataset.sign
+    });
+  }
+
+  render() {
+    console.log(this.state);
+    const select = this.state.data.map(item => {
+      return (
+        <option
+          key={item.name}
+          data-code={item.code}
+          data-sign={item.sign}
+          data-sell={item.sellRate}
+          value={item.name}>{item.name}</option>
+      );
+    }).slice(1);
+
+    const { data, name, rate, code, sign } = this.state;
+
     return (
       <div>
         <header>
@@ -19,55 +52,50 @@ class App extends React.Component {
             <div className="col-md-6 col-md-offset-3">
               <h2>Select Currency</h2>
               <p>
-                {
-                  //Select currency
-                }
-                <select>
-                  <option value="A">Option A</option>
-                  <option value="B">Option B</option>
+                <select onChange={(event) => this.handleChange(event)}>
+                  {
+                    select
+                  }
                 </select>
               </p>
             </div>
           </div>
-          
+
           <div className="row">
             <div className="col-sm-6 currency-from-input">
               <h3 className="currency-flag AUD">Australian Dollars</h3>
               {
-                  //Currency A input
+                //Currency A input
               }
               <div className="input-group">
                 <span className="input-group-addon">$</span>
-                <input type="number" defaultValue={0} className="form-control" aria-describedby="basic-addon2" step="1" pattern="\d\.\d{2}"  />
+                <input type="number" defaultValue={0} className="form-control" aria-describedby="basic-addon2" step="1" pattern="\d\.\d{2}" />
                 <span className="input-group-addon" id="basic-addon2">AUD</span>
               </div>
 
             </div>
             <div className="col-sm-6 currency-to-input">
-              <h3 className="currency-flag USD">United States Dollars</h3>
+              <h3 className={`currency-flag ${code}`}>United States Dollars</h3>
               {
-                  //Currency B input
+                //Currency B input
               }
               <div className="input-group">
-                <span className="input-group-addon">$</span>
-                <input type="number" defaultValue={0} className="form-control" aria-describedby="basic-addon3" step="1" pattern="\d\.\d{2}"  />
-                <span className="input-group-addon" id="basic-addon3">USD</span>
+                <span className="input-group-addon">{sign}</span>
+                <input type="number" defaultValue={0} className="form-control" aria-describedby="basic-addon3" step="1" pattern="\d\.\d{2}" />
+                <span className="input-group-addon" id="basic-addon3">{code}</span>
               </div>
 
             </div>
           </div>
           <div className="row">
             <div className="col-sm-12">
-              {
-                  //Update to currently selected currency
-              }
               <p>
-                Exchange Rate $ 1 AUD = $ 0.7041 USD
+                Exchange Rate $ 1 AUD = {sign} {rate} {code}
               </p>
             </div>
           </div>
         </div>
-      </div>
+      </div >
     )
   }
 }
