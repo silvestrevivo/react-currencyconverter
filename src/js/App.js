4294e7d78eabb7b370ace3ba2490f493a3b474ee
silvestrevivo <silvestrevivomillan@gmail.com>
2018-02-14 07:36:34 +0100

    reducing inputs to one function

--- src/js/App.js
+++ src/js/App.js
@@ -2,24 +2,21 @@ import React from 'react';
 import image from '../images/cash-calculator.svg';
 import data from './data/Data';
 
-
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       data: data.currencies,
-      name: 'United States',
-      rate: 0.7041,
-      code: 'USD',
-      sign: '$',
+      name: data.currencies[1]['name'],
+      rate: data.currencies[1].sellRate,
+      code: data.currencies[1].code,
+      sign: data.currencies[1].sign,
       inputA: 0,
       inputB: 0
     }
   }
 
-  handleChange(event) {
-    let target = event.target;
+  handleChange(target) {
     this.setState({
       name: target.options[target.selectedIndex].value,
       code: target.options[target.selectedIndex].dataset.code,
@@ -28,25 +25,21 @@ class App extends React.Component {
     });
   }
 
-  inputA(event) {
-    //console.log(event.target.value);
-    this.setState({
-      inputA: event.target.value,
-      inputB: event.target.value * this.state.rate
-    });
-
-  }
-
-  inputB(event) {
-    //console.log(event.target.value);
-    this.setState({
-      inputB: event.target.value,
-      inputA: event.target.value / this.state.rate
-    });
+  inputHandler(target) {
+    if (target.className.includes('inputA')) {
+      this.setState({
+        inputA: target.value,
+        inputB: target.value * this.state.rate
+      });
+    } else {
+      this.setState({
+        inputB: target.value,
+        inputA: target.value / this.state.rate
+      });
+    }
   }
 
   render() {
-    console.log(this.state);
     const select = this.state.data.map(item => {
       return (
         <option
@@ -60,9 +53,6 @@ class App extends React.Component {
 
     const { data, name, rate, code, sign, inputA, inputB } = this.state;
 
-    console.log('inputA', inputA);
-    console.log('inputB', inputB);
-
     return (
       <div>
         <header>
@@ -74,7 +64,7 @@ class App extends React.Component {
             <div className="col-md-6 col-md-offset-3">
               <h2>Select Currency</h2>
               <p>
-                <select onChange={(event) => this.handleChange(event)}>
+                <select onChange={(event) => this.handleChange(event.target)}>
                   {
                     select
                   }
@@ -86,40 +76,34 @@ class App extends React.Component {
           <div className="row">
             <div className="col-sm-6 currency-from-input">
               <h3 className="currency-flag AUD">Australian Dollars</h3>
-              {
-                //Currency A input
-              }
+
               <div className="input-group">
                 <span className="input-group-addon">$</span>
                 <input
                   type="number"
-                  defaultValue={0}
-                  className="form-control"
+                  className="form-control inputA"
                   aria-describedby="basic-addon2"
                   step="1"
                   pattern="\d\.\d{2}"
                   value={this.state.inputA}
-                  onChange={(event) => this.inputA(event)} />
+                  onChange={(event) => this.inputHandler(event.target)} />
                 <span className="input-group-addon" id="basic-addon2">AUD</span>
               </div>
 
             </div>
             <div className="col-sm-6 currency-to-input">
               <h3 className={`currency-flag ${code}`}>{name}</h3>
-              {
-                //Currency B input
-              }
+
               <div className="input-group">
                 <span className="input-group-addon">{sign}</span>
                 <input
                   type="number"
-                  defaultValue={0}
                   className="form-control"
                   aria-describedby="basic-addon3"
                   step="1"
                   pattern="\d\.\d{2}"
                   value={this.state.inputB}
-                  onChange={(event) => this.inputB(event)} />
+                  onChange={(event) => this.inputHandler(event.target)} />
                 <span className="input-group-addon" id="basic-addon3">{code}</span>
               </div>
 
